0141ba105d6030ceaa565e0a53994ab4baabe811
Sowmya-Bollumalla <bollumallasowmya@gmail.com>
2025-10-24 21:22:03 +0530

    Initial commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class Constants {
+  static const String apiKey = '79e2b728197913edf1210b5a1a0851ee';
+  static const String apiKey2 = '06d843c6a6bb508f7b62fc868f116adb';
+}
